80aa20db832e21c7e40719222594f69871d77424
OlgaStrelk <86570826+OlgaStrelk@users.noreply.github.com>
2022-05-16 18:02:14 +0300

    open Edit Profile popup

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,9 +2,10 @@ import React, { useState } from "react";
 import PopupWithForm from "./PopupWithForm";
 
 function Main() {
-  let [isModalOpen, setModalIsOpen] = useState(false);
+  let [isEditProfilePopupOpen, setEditProfilePopupOpen] = useState(false);
+
   function handleEditAvatarClick() {
-    setModalIsOpen(true);
+    setEditProfilePopupOpen(true);
   }
 
   // const handleEditProfileClick = () => {
@@ -46,9 +47,155 @@ function Main() {
 
       <section className="cards"></section>
 
-      {isModalOpen && <PopupWithForm />}
+      {isEditProfilePopupOpen && <PopupWithForm
+      title="Редактировать профиль"
+      name="profile"
+      isEditProfilePopupOpen>
+<input
+  required
+  minLength="2"
+  maxLength="40"
+  type="text"
+  autoComplete="off"
+  name="name"
+  id="form-field-name"
+  className="popup__field popup__field_type_name"
+  placeholder="Ваше имя"
+/>
+
+<span id="form-field-name-error" className="popup__error"></span>
+
+<input
+  required
+  minLength="2"
+  maxLength="200"
+  type="text"
+  autoComplete="off"
+  name="occupation"
+  id="form-field-job"
+  className="popup__field popup__field_type_job"
+  placeholder="Чем вы занимаетесь?"
+/>
+
+<span id="form-field-job-error" className="popup__error"></span>
+
+</PopupWithForm>}
     </main>
   );
 }
 
 export default Main;
+
+/*<div className="popup popup_type_add-card">
+        <div className="popup__container">
+          <button
+            type="button"
+            className="popup__close popup__close_type_add-card"
+            aria-label="Закрыть"
+          ></button>
+
+          <h2 className="popup__title">Новое место</h2>
+
+          <form
+            name="edit-form"
+            className="popup__form popup__form_type_add-card"
+            noValidate
+          >
+            <input
+              required
+              minLength="2"
+              maxLength="30"
+              type="text"
+              name="place"
+              className="popup__field"
+              id="form-field-place"
+              placeholder="Название"
+            />
+
+            <span id="form-field-place-error" className="popup__error"></span>
+
+            <input
+              required
+              type="url"
+              name="link"
+              className="popup__field"
+              id="form-field-link"
+              placeholder="Ссылка на картинку"
+            />
+
+            <span id="form-field-link-error" className="popup__error"></span>
+
+            <button
+              type="submit"
+              className="popup__submit"
+              aria-label="Создать"
+            >
+              Создать
+            </button>
+          </form>
+        </div>
+      </div>
+
+      <div className="popup popup_type_delete-card">
+        <div className="popup__container">
+          <button
+            type="button"
+            className="popup__close"
+            aria-label="Закрыть"
+          ></button>
+
+          <h2 className="popup__title">Вы уверены?</h2>
+          <form
+            name="delete-form"
+            className="popup__form popup__form_type_delete-card"
+            noValidate
+          >
+            <button
+              type="submit"
+              className="popup__submit"
+              aria-label="Создать"
+            >
+              Да
+            </button>
+          </form>
+        </div>
+      </div>
+
+      <div className="popup popup_type_edit-user-pic">
+        <div className="popup__container">
+          <button
+            type="button"
+            className="popup__close"
+            aria-label="Закрыть"
+          ></button>
+
+          <h2 className="popup__title">Обновить аватар</h2>
+          <form
+            name="delete-form"
+            className="popup__form"
+            noValidate
+          >
+            <input
+              required
+              type="url"
+              name="link"
+              className="popup__field"
+              id="form-field-user-pic"
+              placeholder="Ссылка на аватар"
+            />
+
+            <span
+              id="form-field-user-pic-error"
+              className="popup__error"
+            ></span>
+
+            <button
+              type="submit"
+              className="popup__submit"
+              aria-label="Создать"
+            >
+              Сохранить
+            </button>
+          </form>
+        </div>
+      </div>*/
--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,175 +1,28 @@
 import React from "react";
 
-const PopupWithForm = ({ isModalOpen, setModalIsOpen }) => {
+const PopupWithForm = (props) => {
+  const { title, name, isEditProfilePopupOpen, children } = props;
   return (
     <>
       <div
-        className={isModalOpen ? "popup popup_is-opened" : "popup popup_type_profile"}
-        onClick={() => setModalIsOpen(false)}
+        className={`popup poup_type_${name} ${
+          isEditProfilePopupOpen && "popup_is-opened"
+        }`}
       >
-        <div className="popup__container" onClick={e => e.stopPropagation()}>
-          <button
-            type="button"
-            className="popup__close popup__close_type_profile"
-            aria-label="Закрыть"
-          ></button>
-
-          <h2 className="popup__title">Редактировать профиль</h2>
-
-          <form
-            name="edit-form"
-            className="popup__form popup__form_type_profile"
-            noValidate
-          >
-            <input
-              required
-              minLength="2"
-              maxLength="40"
-              type="text"
-              autoComplete="off"
-              name="name"
-              id="form-field-name"
-              className="popup__field popup__field_type_name"
-              placeholder="Ваше имя"
-            />
-
-            <span id="form-field-name-error" className="popup__error"></span>
-
-            <input
-              required
-              minLength="2"
-              maxLength="200"
-              type="text"
-              autoComplete="off"
-              name="occupation"
-              id="form-field-job"
-              className="popup__field popup__field_type_job"
-              placeholder="Чем вы занимаетесь?"
-            />
-
-            <span id="form-field-job-error" className="popup__error"></span>
-
-            <button
-              type="submit"
-              className="popup__submit"
-              aria-label="Сохранить"
-            >
-              Сохранить
-            </button>
-          </form>
-        </div>
-      </div>
-
-      <div className="popup popup_type_add-card">
         <div className="popup__container">
           <button
             type="button"
-            className="popup__close popup__close_type_add-card"
+            className="popup__close popup__close_type_${name}"
             aria-label="Закрыть"
           ></button>
 
-          <h2 className="popup__title">Новое место</h2>
-
+          <h2 className="popup__title">{title}</h2>
           <form
-            name="edit-form"
-            className="popup__form popup__form_type_add-card"
+            name="${name}"
+            className="popup__form popup__form_type_${name}"
             noValidate
           >
-            <input
-              required
-              minLength="2"
-              maxLength="30"
-              type="text"
-              name="place"
-              className="popup__field"
-              id="form-field-place"
-              placeholder="Название"
-            />
-
-            <span id="form-field-place-error" className="popup__error"></span>
-
-            <input
-              required
-              type="url"
-              name="link"
-              className="popup__field"
-              id="form-field-link"
-              placeholder="Ссылка на картинку"
-            />
-
-            <span id="form-field-link-error" className="popup__error"></span>
-
-            <button
-              type="submit"
-              className="popup__submit"
-              aria-label="Создать"
-            >
-              Создать
-            </button>
-          </form>
-        </div>
-      </div>
-
-      <div className="popup popup_type_delete-card">
-        <div className="popup__container">
-          <button
-            type="button"
-            className="popup__close popup__close_type_delete-card"
-            aria-label="Закрыть"
-          ></button>
-
-          <h2 className="popup__title">Вы уверены?</h2>
-          <form
-            name="delete-form"
-            className="popup__form popup__form_type_delete-card"
-            noValidate
-          >
-            <button
-              type="submit"
-              className="popup__submit"
-              aria-label="Создать"
-            >
-              Да
-            </button>
-          </form>
-        </div>
-      </div>
-
-      <div className="popup popup_type_edit-user-pic">
-        <div className="popup__container">
-          <button
-            type="button"
-            className="popup__close popup__close_type_edit-user-pic"
-            aria-label="Закрыть"
-          ></button>
-
-          <h2 className="popup__title">Обновить аватар</h2>
-          <form
-            name="delete-form"
-            className="popup__form popup__form_type_edit-user-pic"
-            noValidate
-          >
-            <input
-              required
-              type="url"
-              name="link"
-              className="popup__field"
-              id="form-field-user-pic"
-              placeholder="Ссылка на аватар"
-            />
-
-            <span
-              id="form-field-user-pic-error"
-              className="popup__error"
-            ></span>
-
-            <button
-              type="submit"
-              className="popup__submit"
-              aria-label="Создать"
-            >
-              Сохранить
-            </button>
+            {children}
           </form>
         </div>
       </div>
