9a0cac1aa89d85f714d1bd6169e8c05dafcd3154
OlgaStrelk <86570826+OlgaStrelk@users.noreply.github.com>
2022-05-18 12:57:40 +0300

    improve the code

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,36 +1,40 @@
 import React from "react";
 
-const PopupWithForm = (props) => {
-  const { title, name, isOpen, onClose, children, buttonText } = props;
+const PopupWithForm = ({
+  title,
+  name,
+  isOpen,
+  onClose,
+  children,
+  buttonText,
+}) => {
   return (
-    <>
-      <div className={`popup poup_type_${name} ${isOpen && "popup_is-opened"}`}>
-        <div className="popup__container">
-          <button
-            type="button"
-            className="popup__close popup__close_type_${name}"
-            aria-label="Закрыть"
-            onClick={onClose}
-          ></button>
+    <div className={`popup poup_type_${name} ${isOpen && "popup_is-opened"}`}>
+      <div className="popup__container">
+        <button
+          type="button"
+          className="popup__close popup__close_type_${name}"
+          aria-label="Закрыть"
+          onClick={onClose}
+        ></button>
 
-          <h2 className="popup__title">{title}</h2>
-          <form
-            name="${name}"
-            className="popup__form popup__form_type_${name}"
-            noValidate
+        <h2 className="popup__title">{title}</h2>
+        <form
+          name="${name}"
+          className="popup__form popup__form_type_${name}"
+          noValidate
+        >
+          {children}
+          <button
+            type="submit"
+            className="popup__submit"
+            aria-label={buttonText}
           >
-            {children}
-            <button
-                type="submit"
-                className="popup__submit"
-                aria-label={buttonText}
-              >
-                {buttonText}
-              </button>
-          </form>
-        </div>
+            {buttonText}
+          </button>
+        </form>
       </div>
-    </>
+    </div>
   );
 };
 
