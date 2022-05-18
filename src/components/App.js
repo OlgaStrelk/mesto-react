9893e86cbeba4e189ace9916054e77eff96c44cd
OlgaStrelk <86570826+OlgaStrelk@users.noreply.github.com>
2022-05-18 12:54:27 +0300

    delete double code with popups opening

--- src/components/App.js
+++ src/components/App.js
@@ -42,99 +42,95 @@ function App() {
             onCardClick={handleCardClick}
             selectedCard={selectedCard}
           />
-          {isEditProfilePopupOpen && (
-            <PopupWithForm
-              title="Редактировать профиль"
-              name="profile"
-              isOpen={isEditProfilePopupOpen}
-              onClose={closeAllPopups}
-              buttonText="Сохранить"
-            >
-              <input
-                required
-                minLength="2"
-                maxLength="40"
-                type="text"
-                autoComplete="off"
-                name="name"
-                id="form-field-name"
-                className="popup__field popup__field_type_name"
-                placeholder="Ваше имя"
-              />
 
-              <span id="form-field-name-error" className="popup__error"></span>
+          <PopupWithForm
+            title="Редактировать профиль"
+            name="profile"
+            isOpen={isEditProfilePopupOpen}
+            onClose={closeAllPopups}
+            buttonText="Сохранить"
+          >
+            <input
+              required
+              minLength="2"
+              maxLength="40"
+              type="text"
+              autoComplete="off"
+              name="name"
+              id="form-field-name"
+              className="popup__field popup__field_type_name"
+              placeholder="Ваше имя"
+            />
 
-              <input
-                required
-                minLength="2"
-                maxLength="200"
-                type="text"
-                autoComplete="off"
-                name="occupation"
-                id="form-field-job"
-                className="popup__field popup__field_type_job"
-                placeholder="Чем вы занимаетесь?"
-              />
+            <span id="form-field-name-error" className="popup__error"></span>
 
-              <span id="form-field-job-error" className="popup__error"></span>
-            </PopupWithForm>
-          )}
-          {isAddPlacePopupOpen && (
-            <PopupWithForm
-              title="Новое место"
-              name="add-card"
-              isOpen={isAddPlacePopupOpen}
-              onClose={closeAllPopups}
-              buttonText="Создать"
-            >
-              <input
-                required
-                minLength="2"
-                maxLength="30"
-                type="text"
-                name="place"
-                className="popup__field"
-                id="form-field-place"
-                placeholder="Название"
-              />
+            <input
+              required
+              minLength="2"
+              maxLength="200"
+              type="text"
+              autoComplete="off"
+              name="occupation"
+              id="form-field-job"
+              className="popup__field popup__field_type_job"
+              placeholder="Чем вы занимаетесь?"
+            />
 
-              <span id="form-field-place-error" className="popup__error"></span>
+            <span id="form-field-job-error" className="popup__error"></span>
+          </PopupWithForm>
 
-              <input
-                required
-                type="url"
-                name="link"
-                className="popup__field"
-                id="form-field-link"
-                placeholder="Ссылка на картинку"
-              />
+          <PopupWithForm
+            title="Новое место"
+            name="add-card"
+            isOpen={isAddPlacePopupOpen}
+            onClose={closeAllPopups}
+            buttonText="Создать"
+          >
+            <input
+              required
+              minLength="2"
+              maxLength="30"
+              type="text"
+              name="place"
+              className="popup__field"
+              id="form-field-place"
+              placeholder="Название"
+            />
 
-              <span id="form-field-link-error" className="popup__error"></span>
-            </PopupWithForm>
-          )}
-          {isEditAvatarPopupOpen && (
-            <PopupWithForm
-              title="Обновить аватар"
-              name="edit-user-pic"
-              isOpen={isEditAvatarPopupOpen}
-              onClose={closeAllPopups}
-              buttonText="Сохранить"
-            >
-              <input
-                required
-                type="url"
-                name="link"
-                className="popup__field"
-                id="form-field-user-pic"
-                placeholder="Ссылка на аватар"
-              />
+            <span id="form-field-place-error" className="popup__error"></span>
 
-              <span
-                id="form-field-user-pic-error"
-                className="popup__error"
-              ></span>
-            </PopupWithForm>
-          )}
+            <input
+              required
+              type="url"
+              name="link"
+              className="popup__field"
+              id="form-field-link"
+              placeholder="Ссылка на картинку"
+            />
+
+            <span id="form-field-link-error" className="popup__error"></span>
+          </PopupWithForm>
+          <PopupWithForm
+            title="Обновить аватар"
+            name="edit-user-pic"
+            isOpen={isEditAvatarPopupOpen}
+            onClose={closeAllPopups}
+            buttonText="Сохранить"
+          >
+            <input
+              required
+              type="url"
+              name="link"
+              className="popup__field"
+              id="form-field-user-pic"
+              placeholder="Ссылка на аватар"
+            />
+
+            <span
+              id="form-field-user-pic-error"
+              className="popup__error"
+            ></span>
+          </PopupWithForm>
           <ImagePopup selectedCard={selectedCard} onClose={closeAllPopups} />
           <Footer />
         </div>
