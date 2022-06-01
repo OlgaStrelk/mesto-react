d994f541aa005b90726543766a913f095f260a1d
Strelkova Olga <strelod@yandex.ru>
2022-06-01 17:20:41 +0300

    refact popup add card remove current user import

--- src/components/AddPlacePopup.js
+++ src/components/AddPlacePopup.js
@@ -1,9 +1,7 @@
 import React, { useContext, useState } from "react";
 import PopupWithForm from "./PopupWithForm";
-import { CurrentUserContext } from "../contexts/CurrentUserContext";
 
 function AddPlacePopup({ isOpen, onClose, onAddCard }) {
-  const currentUser = useContext(CurrentUserContext);
 
   const [name, setName] = useState("");
   const [link, setLink] = useState("");
