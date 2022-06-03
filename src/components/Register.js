98b297147659cc710f4816641c25f150bea35ef7
Strelkova Olga <strelod@yandex.ru>
2022-06-03 17:57:55 +0300

    feat: render all routes

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,5 +1,9 @@
-import React, { Link } from "react";
+import React from "react";
+import { Link } from "react-router-dom";
+
 function Register() {
+
+
   return (
     <div className="auth-form">
       <form className="auth-form__form">
@@ -26,16 +30,17 @@ function Register() {
             />
           </label>
         </div>
-        <div className="auth-form-container"></div>
-        <button className="auth-form__button" type="submit">
-          Зарегистрироваться
-        </button>
-        <p className="auth-form__question">
-          Уже зарегистрированы?
-          <Link className="auth-form__link" to="/sign-in">
-            Войти
-          </Link>
-        </p>
+        <div className="auth-form__container">
+          <button className="auth-form__button" type="submit">
+            Зарегистрироваться
+          </button>
+          <p className="auth-form__text">
+            Уже зарегистрированы?{" "}
+            <Link className="auth-form__link" to="/sign-in">
+              Войти
+            </Link>
+          </p>
+        </div>
       </form>
     </div>
   );
